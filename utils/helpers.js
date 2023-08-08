3afb3d8be9f7f025e0a2b84a518ca8df68a2f2f7
Saloni0412 <pshashikantb@gmail.com>
2023-08-08 00:15:05 -0400

    this adds code chjanges

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,10 +1,5 @@
-const withAuth = (req, res, next) => {
-    if (!req.session.userId) {
-      res.redirect("/login");
-    } else {
-      next();
-    }
-  };
-  
-  module.exports = withAuth;
-  
+module.exports = {
+  format_date: date => {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+  }
+};
